64a14f088e81ee371c1236ce6b9f2c131647f495
salil <salilpa007@gmail.com>
2014-06-22 19:46:45 +0530

    Complete JS for button interaction implemented prediction button and status button

--- pnrme/js/webapp.js
+++ pnrme/js/webapp.js
@@ -1,59 +1,22 @@
-OpenWeatherMapDailyRequest = function(options) {
-	this.url = options.url ? options.url : 'http://api.openweathermap.org/data/2.5/weather';
-	this.units = options.units ? options.units : 'metric';
-	this.city = options.city ? options.city : null;
-	this.lat = options.lat ? options.lat : null;
-	this.lon = options.lon ? options.lon : null;
-	this.cityId = options.cityId ? options.cityId : null;
+PredictorRequest = function(options) {
+    this.predict = options.predict;
+	this.url = this.predict == true ? 'http://pnr.me/restPredictor/' : 'http://pnr.me/restStatus/';
+	this.pnr = options.pnr ? options.pnr : '1234567890';
 	this.response = null;
-	
+
 	this.getURL = function(){
-		if (this.citiId != null){
-			return this.url + "?id=" + this.cityId + "&units=" + this.units;
-		}else if (this.city != null){
-			return this.url + "?q=" + this.city + "&units=" + this.units;
-		}else if( this.lat != null && this.lon != null){
-			return this.url + "?lat=" + this.lat + "&lon=" + this.lon + 
-				"&units=" + this.units;
-		}else{
+		if (this.pnr != null){
+			return this.url + this.pnr;
+		}else {
 			throw new Error("Incorrect Request");
 		}
 	};
-	
+
 	this.call = function(){
 		apiRequest(this);
 	};
 };
 
-OpenWeatherMapForecastRequest = function(options) {
-	this.url = options.url ? options.url : 'http://api.openweathermap.org/data/2.5/forecast/daily';
-	this.units = options.units ? options.units : 'metric';
-	this.days = options.days ? options.days : 5;
-	this.city = options.city ? options.city : null;
-	this.lat = options.lat ? options.lat : null;
-	this.lon = options.lon ? options.lon : null;
-	this.cityId = options.cityId ? options.cityId : null;
-	this.response = null;
-	
-	this.getURL = function(){
-		if (this.citiId != null){
-			return this.url + "?id=" + this.cityId + "&units=" + 
-			this.units + "&cnt=" + this.days;
-		}else if (this.city != null){
-			return this.url + "?q=" + this.city + "&units=" + 
-				this.units + "&cnt=" + this.days;
-		}else if( this.lat != null && this.lon != null){
-			return this.url + "?lat=" + this.lat + "&lon=" + this.lon + 
-				"&units=" + this.units + "&cnt=" + this.days;
-		}else{
-			throw new Error("Incorrect Request");
-		}
-	};
-	
-	this.call = function(){
-		apiRequest(this);
-	};
-};
 
 function apiRequest(request){
 	var xhr = new XMLHttpRequest({mozSystem: true});
@@ -66,23 +29,17 @@ function apiRequest(request){
     };
 
     xhr.onerror = function () {
-    	alert("Failed to reach OpenWeatherMap.org API server");
+    	alert("Failed to reach pnr.me API server");
     };
     
     xhr.send();
 }
 
-function renderResponse(request, dailyRequest){
+function renderResponse(predictionResult){
 	$('#accordion').html('');
 	$('#results-container').html('');
-	$('#location-display').val(request.response.city.name);
-	$('#location-display').data('city-id', request.response.city.id);
 	
-	renderDailyResult(dailyRequest);
-	
-	$.each(request.response.list, function(i, el){
-		renderAccordion(i, el, request.units);
-	});
+	renderPrediction(predictionResult);
 }
 
 function renderProgressBar(time){
@@ -108,235 +65,81 @@ function renderProgressBar(time){
 	$('#progress').html(html).show();
 }
 
-function renderDailyResult(request){
-	var date = new Date();
-	var day = date.getHours() <= 18 && date.getHours() > 6; // if we had sunset time
-	var unit = request.units === 'metric' ? '°C' : '°F';
-	var weather = request.response.weather[0];
-	var iconCode = weather.icon.substring(0,2);
-	var icon = day ? iconCode + "d.png" : iconCode + "n.png";
-	var html = '<div class="well well-ffos"><img src="http://openweathermap.org/img/w/';
-	html += icon;
-	html += '" align="left"><div class="temp-container"><span class="temp">';
-	html += request.response.main.temp.toFixed() + unit;
-	html += '</span><span class="min">';
-	html += request.response.main.temp_min.toFixed() + unit;
-	html += '</span><span class="max">';
-	html += request.response.main.temp_max.toFixed() + unit;
-	html += '</span></div></div>';
+function renderPrediction(request){
+    var message = '';
+    if (request.response.status == 'error'){
+        message = '<div class="alert alert-error">' + request.response.message[0] + '</div>';
+    } else if (request.predict){
+        message += '<div class="alert alert-success">';
+        for (var i= 0; i < request.response.message.length; i++){
+            message += request.response.message[i][0] + ' : ' + request.response.message[i][1] + '<br>';
+        }
+        message += '</div>';
+    } else{
+        message += '<div><table class="table table-bordered table-striped"><thead>';
+        message += '<tr><th>no</th><th>booking</th><th>current</th></tr></thead><tbody>';
+        for (var i= 0; i < request.response.message.passenger_status.length; i++){
+            message += '<tr><td>'+ i+1 + '</td><td>' + request.response.message.passenger_status[i].booking_status + '</td><td>' + request.response.message.passenger_status[i].current_status + '</td></tr>';
+        }
+        message += '</tbody></table><table class="table table-bordered table-striped"><tbody>';
+        message += '<tr><td>from</td><td>' + request.response.message.from + '</td></tr>';
+        message += '<tr><td>to</td><td>' + request.response.message.to + '</td></tr>';
+        message += '<tr><td>class</td><td>' + request.response.message.class + '</td></tr>';
+        message += '<tr><td>train number</td><td>' + request.response.message.train_number + '</td></tr>';
+        message += '<tr><td>train name</td><td>' + request.response.message.train_name + '</td></tr>';
+        message += '<tr><td>boarding date</td><td>' + request.response.message.boarding_date + '</td></tr>';
+        message += '<tr><td>boarding point</td><td>' + request.response.message.boarding_point + '</td></tr>';
+        message += '<tr><td>reserved up to</td><td>' + request.response.message.reserved_upto + '</td></tr>';
+        message += '</tbody></table>';
+        message += '</div>';
+    }
+	var html = '';
+	html += message;
 	$('#results-container').html(html);
 }
 
-function renderAccordion(index, obj, units){
-	var date = new Date(obj.dt * 1000);
-	var day = date.getHours() <= 18 && date.getHours() > 6; // if we had sunset time
-	var unit = units === 'metric' ? '°C' : '°F';
-	var weather = obj.weather[0];
-	var iconCode = weather.icon.substring(0,2);
-	var icon = day ? iconCode + "d.png" : iconCode + "n.png";
-	var acc = '<div class="accordion-group-ffos"><div class="accordion-heading">';
-	acc += '<a class="accordion-toggle" data-toggle="collapse" ';
-	acc += 'data-parent="#accordion" href="#collapse-';
-	acc += index;
-	acc += '"><img src="http://openweathermap.org/img/w/';
-	acc += icon;
-	acc += '" align="left">';
-	acc += '<div><div class="date">';
-	acc += date.toUTCString().substring(0,17).trim(); // this will fail for some edge case
-	acc += '</div><div class="weather">';
-	acc += weather.main + ' <span class="temp">' + obj.temp.day.toFixed() + unit + '</span>';
-	acc += '</div></div></a></div><div id="collapse-';
-	acc += index;
-	acc += '" class="accordion-body collapse"><div class="accordion-inner">';
-	acc += '<p>Minimum: <span class="min">' + obj.temp.min.toFixed() + unit + '</span></p>';
-	acc += '<p>Maximum: <span class="max">' + obj.temp.max.toFixed() + unit + '</span></p>';
-	acc += '<p>Pressure: ' + obj.pressure + 'hpa</p>';
-	acc += '<p>Humidity: ' + obj.humidity + '%</p>';
-	acc += '<p>Wind: ' + obj.speed + 'm/s, ' + obj.deg + '°</p>';
-	acc += '</div></div></div>';
-	acc += '';
-	$('#accordion').append(acc);
-}
+function getData(options){
+    var predictRequest = new PredictorRequest(options);
+    predictRequest.call();
 
-function clearSearchOptions(){
-	window.lastCall = null;
-	$('#location-display').removeData('lat').removeData('lon').removeData('city-id');
-}
-
-var debugDailyResponse = {
-	name: 'debug',
-	dt: 1369147271,
-	id: 2643743,
-	cod: 200,
-	main: {
-		temp: 286.48,
-		pressure: 1017,
-		temp_min: 284.82,
-		temp_max: 288.15,
-		humidity: 74
-	},
-    weather: [
-        {
-        	id: 802,
-        	main: "Clouds",
-        	description: "scattered clouds",
-        	icon: "03d"
+    var tries = 0;
+    var intervalID = window.setInterval(function(){
+        if (predictRequest.response != null){
+            console.log("Got the responses");
+            try{
+                renderResponse(predictRequest);
+            }catch(err){
+                alert("Woops. Something failed. Try again later.");
+                console.log(err);
+            }finally{
+                window.clearInterval(intervalID);
+                $('#progress').hide();
+            }
+        }else if ( tries === 20){
+            $('#progress').hide();
+            alert("Failed to reach pnr.me API server after 20 seconds");
+            window.clearInterval(intervalID);
+        }else{
+            console.log("Checking for response try " + tries);
+            tries++;
+            renderProgressBar(tries);
         }
-    ]
-};
+    }, 1000);
+}
 
-var debugForecastResponse = {
-	city: {
-		id: 524901,
-		name: "debug",
-	},
-	list: [
-		{
-			dt: 1369126800,
-			temp: {
-				day: 17.17,
-				min: 13.39,
-				max: 17.17,
-			},
-			pressure: 1005.18,
-			humidity: 66,
-			weather: [
-				{
-					id: 803,
-					main: "Clouds",
-					icon: "04d"
-				}
-			],
-			speed: 3.22,
-			deg: 112,
-		},{
-			dt: 1369213200,
-			temp: {
-				day: 17.17,
-				min: 13.39,
-				max: 17.17,
-			},
-			pressure: 1005.18,
-			humidity: 66,
-			weather: [
-				{
-					id: 803,
-					main: "Clouds",
-					icon: "04d"
-				}
-			],
-			speed: 3.22,
-			deg: 112,
-		},
-    ]
-};
+
 
 (function() {
 	// set a global var with the last request date
 	// with this we can check when to call the API again
-	window.lastCall = null; 
-	
-	var locationDisplay = $('#location-display');
-	
-	$(locationDisplay).click(function(){
-		$(this).val('');
+
+    $('#status-button').click(function(){
+        var pnr = $('#pnr-number').val();
+        getData({'pnr': pnr, 'predict': false});
+    });
+	$('#predict-button').click(function(){
+        var pnr = $('#pnr-number').val();
+        getData({'pnr': pnr, 'predict': true});
 	});
-	
-	$('#location-button').click(function(){
-		$('#location-button').prop('disabled', true);
-		navigator.geolocation.getCurrentPosition(function (position) {
-			clearSearchOptions();
-			var location = position.coords.latitude + ", " + position.coords.longitude;
-			locationDisplay.val(location);
-			locationDisplay.data('lat', position.coords.latitude);
-			locationDisplay.data('lon', position.coords.longitude);
-			$('#location-button').prop('disabled', false);
-		},function (position) {
-			alert("Failed to get your current location.");
-			$('#location-button').prop('disabled', false);
-        });
-	});
-	
-	locationDisplay.change(function(){
-		clearSearchOptions();
-	});
-	
-	$('#search-button').click(function(){
-		
-		var now = new Date();
-		if( window.lastCall != null ){
-			var interval = now - window.lastCall;
-			if (interval < 600000){ //10 minutes
-				console.log("No need to call the API again");
-				var timeleft = 10 - (interval / 60000 );
-				alert("To reduce load on the OpenWeatherMap.org API you're only allowed to check the weather every 10 minutes. Please wait " + timeleft.toFixed() + " minutes and try again");
-				return false;
-			}
-		}
-		
-		var dailyRequest = new OpenWeatherMapDailyRequest({});
-		var request = new OpenWeatherMapForecastRequest({});
-		var location = locationDisplay.val();
-		
-		if ( location === 'debug' ){
-			dailyRequest.response = debugDailyResponse;
-			request.response = debugForecastResponse;
-			renderResponse(request, dailyRequest);
-			return false;
-		}
-		
-		if( $('#imperial-button').hasClass('active') ){
-			dailyRequest.units = 'imperial';
-			request.units = 'imperial';
-		}else{
-			dailyRequest.units = 'metric';
-			request.units = 'metric';
-		}
-		
-		if ( locationDisplay.data('city-id') != undefined ){
-			dailyRequest.cityId = locationDisplay.data('city-id');
-			request.cityId = locationDisplay.data('city-id');
-		}
-		
-		if (locationDisplay.data('lat') == null){
-			dailyRequest.city = location;
-			request.city = location;
-		}else{
-			dailyRequest.lat = locationDisplay.data('lat');
-			dailyRequest.lon = locationDisplay.data('lon');
-			request.lat = locationDisplay.data('lat');
-			request.lon = locationDisplay.data('lon');
-		}
-		
-		console.log("Getting Forecast data from " + request.getURL());
-		
-		dailyRequest.call();
-		request.call();
-		
-		var tries = 0;
-		var intervalID = window.setInterval(function(){
-			if (request.response != null && dailyRequest.response != null){
-				console.log("Got the responses");
-				try{
-					renderResponse(request, dailyRequest);
-				}catch(err){
-					alert("Woops. Something failed. Try again later.");
-					console.log(err);
-				}finally{
-					window.clearInterval(intervalID);
-					window.lastCall = new Date();
-					$('#progress').hide();
-				}
-			}else if ( tries === 20){
-				$('#progress').hide();
-				alert("Failed to reach OpenWeatherMap.org API server after 20 seconds");
-				window.clearInterval(intervalID);
-			}else{
-				console.log("Checking for response try " + tries);
-				tries++;
-				renderProgressBar(tries);
-			}
-		}, 1000);
-	});
-	
+
 })();
